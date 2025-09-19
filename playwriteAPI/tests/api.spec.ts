035ec4d30eac6c271540a2b1f4b51f08ef7ed753
mdeokule <madhurade@cybage.com>
2025-09-19 14:18:35 +0530

    added

--- playwriteAPI/tests/api.spec.ts
+++ playwriteAPI/tests/api.spec.ts
@@ -1,12 +1,11 @@
 import { test, expect } from '@playwright/test';
 import axios from 'axios';
 test.describe('ReqRes API Tests', () => {
-
   test('GET list of users', async ({ request }) => {
     const response = await axios.get('https://reqres.in/api/users?page=2');
     console.log('Status:', response.status);
     expect(response.status).toBe(200);
-    //expect(response.).toBeTruthy();
+    //expect(response.).toBeTruthy();ssss
 
     const data = await response.data;
     console.log('Response:', data);
@@ -18,12 +17,14 @@ test.describe('ReqRes API Tests', () => {
   test('POST create a new user', async ({ request }) => {
     const newUser = {
       name: 'John Doe',
-      job: 'QA Engineer'
+      job: 'QA Engineer',
     };
 
-    const response = await axios.post('https://reqres.in/api/users', newUser,{headers: {
-        'x-api-key':'reqres-free-v1'
-      }});
+    const response = await axios.post('https://reqres.in/api/users', newUser, {
+      headers: {
+        'x-api-key': 'reqres-free-v1',
+      },
+    });
     expect(response.status).toBe(201);
 
     const responseData = await response.data;
@@ -36,11 +37,14 @@ test.describe('ReqRes API Tests', () => {
   test('PUT update user', async ({ request }) => {
     const updatedUser = {
       name: 'John Updated',
-      job: 'Automation Lead'
+      job: 'Automation Lead',
     };
 
-    const response = await axios.put('https://reqres.in/api/users/1', updatedUser,{ headers: {
-        'x-api-key':'reqres-free-v1'}});
+    const response = await axios.put('https://reqres.in/api/users/1', updatedUser, {
+      headers: {
+        'x-api-key': 'reqres-free-v1',
+      },
+    });
     expect(response.status).toBe(200);
 
     const responseData = await response.data;
@@ -50,9 +54,11 @@ test.describe('ReqRes API Tests', () => {
   });
 
   test('DELETE user', async ({ request }) => {
-    const response = await axios.delete('https://reqres.in/api/users/1',{ headers: {
-        'x-api-key':'reqres-free-v1'}});
+    const response = await axios.delete('https://reqres.in/api/users/1', {
+      headers: {
+        'x-api-key': 'reqres-free-v1',
+      },
+    });
     expect(response.status).toBe(204);
   });
-
 });
